0bfdf70b3203156b11220d97f5d8a988644c50f9
Andrew Turgeon <andrew.p.turgeon@gmail.com>
2014-11-12 02:15:37 -0500

    Initial Commit

---
+++ htdocs/templates/purity_iii/local/js/scripts.js
@@ -0,0 +1,21 @@
+;(function($){
+
+ 	$(document).ready(function() {
+ 		$("#splash_home").addClass("loaded");
+// 		$(window).on('scroll', updateMainNavPosition);
+	});
+
+// 	function updateMainNavPosition(){
+// 		var $nav       = $("#t3-mainnav"),
+// 			window_top = $(window).scrollTop(),
+// 			nav_offset = $nav.offset().top - $(window).scrollTop();
+
+// 		if (window_top < 55) {
+// 			$nav.removeClass("navbar-fixed-top").addClass("navbar-absolute-top");
+// 		}
+// 		else if (nav_offset <= 0) {
+// 			$nav.removeClass("navbar-absolute-top").addClass("navbar-fixed-top");
+// 		}
+// 	}
+
+})(jQuery);
