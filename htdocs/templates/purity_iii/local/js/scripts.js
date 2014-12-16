e7ad8e180579b16e3f317f8505828a22b5668135
Andrew Turgeon <andrew.p.turgeon@gmail.com>
2014-12-16 10:15:33 -0500

    Removed commented out js

--- htdocs/templates/purity_iii/local/js/scripts.js
+++ htdocs/templates/purity_iii/local/js/scripts.js
@@ -2,16 +2,6 @@
 
  	$(document).ready(function() {
  		$("#splash_home").addClass("loaded");
-
- 		// $(".contact-link a").click(function(e) {
- 		// 	var $extra_info = $(this).closest(".contact-card").find(".contact-extra-info");
- 		// 	e.preventDefault();
- 		// 	if($extra_info.attr("aria-hidden") === "true") {
- 		// 		$extra_info.attr("aria-hidden", false);
- 		// 	} else {
- 		// 		$extra_info.attr("aria-hidden", true);
- 		// 	}
- 		// });
 	});
 
 })(jQuery);
