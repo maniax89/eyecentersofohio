f03c392619c1fa8e29b0365d0bf2ae2c9c5016f1
Andrew Turgeon <andrew.p.turgeon@gmail.com>
2015-02-11 01:22:45 -0500

    Added in parallax background images for desktop (not iOS devices... yet)

--- htdocs/templates/purity_iii/local/js/scripts.js
+++ htdocs/templates/purity_iii/local/js/scripts.js
@@ -2,6 +2,17 @@
 
  	$(document).ready(function() {
  		$("#splash_home").addClass("loaded");
+
+        $(window).scroll(function() {
+            var maxScroll = -90,
+            	yPos 	  = -($(window).scrollTop() / 5),
+            	coords 	  = '50% '+ yPos + 'px';
+
+            // Move the background
+            if (yPos >= maxScroll){
+            	$('.leading-0').css({ backgroundPosition: coords });
+            }
+        }); 
 	});
 
 })(jQuery);
